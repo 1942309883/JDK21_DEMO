78ce9006210abd31c9373981bec8567d9c95c3ea
junxinlai <laijx@baoz.com.cn>
2024-01-10 15:04:01 +0800

    controller commit test

--- src/main/java/com/bs/SpringJDK21/web/rest/TestResource.java
+++ src/main/java/com/bs/SpringJDK21/web/rest/TestResource.java
@@ -12,14 +12,20 @@ public class TestResource {
 
     @GetMapping("/test2")
     public String test2(){
-        System.out.println("我接到请求了！");
+        System.out.println("我接到请求了2！");
         return "8080：test2知道了";
     }
 
     @GetMapping("/testL")
     public String testL(){
-        System.out.println("我接到请求了！");
+        System.out.println("我接到请求了L！");
         return "8080：testL知道了";
+    }
+
+    @GetMapping("/testP")
+    public String testP(){
+        System.out.println("我接到请求了P！");
+        return "8080：testP知道了";
     }
 
 
